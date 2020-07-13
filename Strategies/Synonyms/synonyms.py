66bb9378d204ec39914c114745c9d609d677e67a
Jolan Huyvaert <jolan.huyvaert@telenet.be>
2020-07-13 15:04:02 +0200

    Added frequency scores to synonyms

--- Strategies/Synonyms/synonyms.py
+++ Strategies/Synonyms/synonyms.py
@@ -7,20 +7,26 @@ def get_synonym_by_word(word, langTag, maxSynonyms): #langTag should be 'eng' fo
    syn = dict()
    for synset in wordnet.synsets(word, lang=langTag):
       for lemma in synset.lemmas(lang=langTag):
-         syn[lemma.name()] = ''   #add the synonyms
-   syn.pop(word.lower(), None)
-   return list(syn)[:maxSynonyms]
+         key = lemma.name()
+         #add the synonyms + frequency
+         if key in syn:
+            syn[key] = syn[key] + 1
+         else:
+            syn[key] = 1   
+   syn.pop(word.lower(), None) #remove the original word from the synonym list
+   syn = {k: v for k, v in sorted(syn.items(), key=lambda item: item[1], reverse=True)} #sort the dictionary by most frequent synonym
+   dictList = [] 
+   for key, value in syn.items(): #convert dictionary to list of key-value pairs ([synonym, frequency_score])
+      temp = [key,value]
+      dictList.append(temp)
+   return list(dictList)[:maxSynonyms] #return 'maxSynonyms' number of synonyms
 
 
 def get_synonym_by_word_list(wordList, langTag, maxSynonyms): #langTag should be 'eng' for English and 'nld' for dutch
    synList = list()
    for word in wordList:
-      syn = dict()
-      for synset in wordnet.synsets(word, lang=langTag):
-         for lemma in synset.lemmas(lang=langTag):
-            syn[lemma.name()] = ''   #add the synonyms
-      syn.pop(word.lower(), None)
-      synList.append(list(syn)[:maxSynonyms])
+      syn = get_synonym_by_word(word, langTag, maxSynonyms)
+      synList.append(syn)
    return [wordList, synList]
 
 print(get_synonym_by_word("Bad", 'eng', 2))
--- Strategies/Synonyms/test_synonyms.py
+++ Strategies/Synonyms/test_synonyms.py
@@ -5,16 +5,16 @@ from synonyms import get_synonym_by_word , get_synonym_by_word_list
 class TestSynonyms(unittest.TestCase):
 
     def test_get_synonym_by_word_eng(self):
-        self.assertEqual(get_synonym_by_word("Bad", 'eng', 2), ['badness', 'big'])
+        self.assertEqual(get_synonym_by_word("Bad", 'eng', 2), [['badly', 2], ['badness', 1]])
 
     def test_get_synonym_by_word_nld(self):
-        self.assertEqual(get_synonym_by_word("Wetenschap", 'nld', 2), ['medeweten', 'perceptie'])
+        self.assertEqual(get_synonym_by_word("Wetenschap", 'nld', 2), [['kennis', 2], ['weten', 2]])
 
     def test_get_synonym_by_word_list_eng(self):
-        self.assertEqual(get_synonym_by_word_list(["Bad", "Science", "Life"], 'eng', 2), [['Bad', 'Science', 'Life'], [['badness', 'big'], ['scientific_discipline', 'skill'], ['living', 'animation']]])
+        self.assertEqual(get_synonym_by_word_list(["Bad", "Science", "Life"], 'eng', 2), [['Bad', 'Science', 'Life'], [[['badly', 2], ['badness', 1]], [['scientific_discipline', 1], ['skill', 1]], [['living', 2], ['animation', 1]]]])
 
     def test_get_synonym_by_word_list_nld(self):
-        self.assertEqual(get_synonym_by_word_list(["Slecht", "Leven", "Onderzoek"], 'nld', 2), [['Slecht', 'Leven', 'Onderzoek'], [['boos', 'kwaad'], ['levensduur', 'levenslang'], ['inspectie', 'monstering']]])
+        self.assertEqual(get_synonym_by_word_list(["Slecht", "Leven", "Onderzoek"], 'nld', 2), [['Slecht', 'Leven', 'Onderzoek'], [[['boos', 1], ['kwaad', 1]], [['voortbestaan', 3], ['overleven', 2]], [['inspectie', 2], ['navraag', 2]]]])
 
 
 if __name__ == '__main__':
