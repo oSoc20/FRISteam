ecbf0b5b9b982128dd9cbb8f836c80a6f020be77
Jolan Huyvaert <jolan.huyvaert@telenet.be>
2020-07-27 13:31:01 +0200

    Updated synonyms + updated unit tests for it

--- Strategies/Synonyms/synonyms.py
+++ Strategies/Synonyms/synonyms.py
@@ -29,12 +29,13 @@ def get_synonym_by_word(word, langTag):
    return syncounterdict
 
 
-def get_synonym_by_word_list(wordList, langTag): 
+def get_synonym_by_word_list(wordList, langTag, max_nr_synonyms=10): 
    """function get_synonym_by_word_list : get synonyms from a list of words in a specified language
 
    Args:
        wordList (list(string)): the list of words to find synonyms for
        langTag (string):  the language of the words in wordList and the synonyms (e.g. 'eng' for English and 'nld' for Dutch)
+       max_nr_synonyms (int): limits the number of synonyms in the output dictionary 
 
    Returns:
        counter dictionary : [synonym_string, frequency_score] in order of highest score
@@ -45,9 +46,13 @@ def get_synonym_by_word_list(wordList, langTag):
 
    normalize_denom = sum(totalsyncounterdict.values())   
    totalsyncounterdict_norm = {k: v/normalize_denom for k, v in totalsyncounterdict.items()}
-   return Counter(totalsyncounterdict_norm)
-
-#print(get_synonym_by_word("Bad", 'eng'))
-#print(get_synonym_by_word("Wetenschap", 'nld', 2))
-#print(get_synonym_by_word_list(["Bad", "Science", "Life"], 'eng'))
-#print(get_synonym_by_word_list(["Slecht", "Leven", "Onderzoek"], 'nld'))
+   normalized_counter_dict = Counter(totalsyncounterdict_norm)
+   result_synonyms_dict = Counter()
+   ctr = 0
+   for keyword, score in normalized_counter_dict.items():
+        if ctr < max_nr_synonyms:
+            result_synonyms_dict[keyword] = score
+            ctr += 1
+        else:
+            break
+   return result_synonyms_dict
--- Tests/Strategies/test_synonyms.py
+++ Tests/Strategies/test_synonyms.py
@@ -9,20 +9,21 @@ PACKAGE_PARENT = '../..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 from Strategies.Synonyms.synonyms import get_synonym_by_word , get_synonym_by_word_list
+from collections import Counter
 
 class TestSynonyms(unittest.TestCase):
 
     def test_get_synonym_by_word_eng(self):
-        self.assertEqual(get_synonym_by_word("Bad", 'eng', 2), [['badly', 0.058823529411764705], ['badness', 0.029411764705882353]])
+        self.assertEqual(get_synonym_by_word("Bad", 'eng'), Counter({'badly': 2, 'badness': 1, 'big': 1, 'tough': 1, 'spoiled': 1, 'spoilt': 1, 'regretful': 1, 'sorry': 1, 'uncollectible': 1, 'risky': 1, 'high-risk': 1, 'speculative': 1, 'unfit': 1, 'unsound': 1, 'forged': 1, 'defective': 1}))
 
     def test_get_synonym_by_word_nld(self):
-        self.assertEqual(get_synonym_by_word("Wetenschap", 'nld', 2), [['kennis', 0.18181818181818182], ['weten', 0.18181818181818182]])
+        self.assertEqual(get_synonym_by_word("Wetenschap", 'nld'), Counter({'kennis': 2, 'weten': 2, 'medeweten': 1, 'perceptie': 1, 'cognitie': 1, 'bagage': 1, 'ervaring': 1}))
 
     def test_get_synonym_by_word_list_eng(self):
-        self.assertEqual(get_synonym_by_word_list(["Bad", "Science", "Life"], 'eng', 2), [['Bad', 'Science', 'Life'], [[['badly', 0.058823529411764705], ['badness', 0.029411764705882353]], [['scientific_discipline', 0.25], ['skill', 0.25]], [['living', 0.07142857142857142], ['animation', 0.03571428571428571]]]])
+        self.assertEqual(get_synonym_by_word_list(["Bad", "Science", "Life"], 'eng', 2), Counter({'badness': 0.030303030303030304, 'big': 0.030303030303030304}))
 
     def test_get_synonym_by_word_list_nld(self):
-        self.assertEqual(get_synonym_by_word_list(["Slecht", "Leven", "Onderzoek"], 'nld', 2), [['Slecht', 'Leven', 'Onderzoek'], [[['boos', 0.25], ['kwaad', 0.25]], [['voortbestaan', 0.07142857142857142], ['overleven', 0.047619047619047616]], [['inspectie', 0.058823529411764705], ['navraag', 0.058823529411764705]]]])
+        self.assertEqual(get_synonym_by_word_list(["Slecht", "Leven", "Onderzoek"], 'nld', 2), Counter({'boos': 0.015625, 'kwaad': 0.015625}))
 
 
 if __name__ == '__main__':
