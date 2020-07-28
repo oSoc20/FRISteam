150c0f57c94a287f3c119bced0fbf75bfea13ff3
Jolan Huyvaert <jolan.huyvaert@telenet.be>
2020-07-28 15:14:21 +0200

    Updated servicemanager documentation

--- ServiceManager/service_manager.py
+++ ServiceManager/service_manager.py
@@ -9,7 +9,14 @@ from Cleanup.datacleaning import clean_data
 import Enricher.enricher as enricher
 
 def process_project(project_obj):
-    #enriched_proj = enricher.enrich_project(project_obj)
+    """This function sends a project object through data cleaning and passes the cleaned object to the enricher
+
+    Args:
+        project_obj (Utils.fris_entities.Project): Instance of Project class 
+
+    Returns:
+        Utils.enricher_entities.ProjectResult: Instance of ProjectResult containing enriched data
+    """
     cleaned_project = clean_data(project_obj)
     print("Service Manager: successful process_project")
     enriched_proj = enricher.enrich_project(cleaned_project)
@@ -17,8 +24,15 @@ def process_project(project_obj):
 
 
 def process_publication(publication_obj):
+    """This function sends a publication object through data cleaning and passes the cleaned object to the enricher
+
+    Args:
+        publication_obj (Utils.fris_entities.Publication): Instance of Publication class 
+
+    Returns:
+        Utils.enricher_entities.PublicationResult: Instance of PublicationResult containing enriched data
+    """
     cleaned_pub = clean_data(publication_obj)
-    #enriched_pub = enricher.enrich_publication(publication_obj)
     print("Service Manager: successful process_publication")
     enriched_pub = enricher.enrich_publication(cleaned_pub)
     return enriched_pub
